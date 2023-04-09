4622a5afbb18b51be400e40232dd48691e4a7b61
Udeorah Frank <franklynudeorah@yahoo.com>
2023-04-09 23:23:31 +0100

    setup index method for listings

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Listing;
 use Illuminate\Http\Request;
 
 class ListingController extends Controller
@@ -13,7 +14,7 @@ class ListingController extends Controller
      */
     public function index()
     {
-        //
+        return Listing::all();
     }
 
     /**

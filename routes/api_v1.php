3045be3eaf65e16feb2561bdd15a7d5e74977943
Udeorah Frank <franklynudeorah@yahoo.com>
2023-04-12 02:26:45 +0100

    enabel sanctum authentication

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -1,11 +1,18 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ListingController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 Route::get('/listings', [ListingController::class, 'index']);
 
-Route::get('/users', [UserController::class, 'index']);
+Route::middleware(['auth:sanctum'])->group(function () {
+  Route::post('/listings/create', [ListingController::class, 'store']);
+  Route::get('/users', [UserController::class, 'index']);
+  Route::get('/user/{id}/{listings}', [ListingController::class, 'index']);
+});

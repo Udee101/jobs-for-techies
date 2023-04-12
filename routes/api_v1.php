8b0c93f659f7c5f708abc704a5bc1a6417caeefb
Udeorah Frank <franklynudeorah@yahoo.com>
2023-04-12 03:56:05 +0100

    add routes for job listings

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -10,9 +10,13 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/listings', [ListingController::class, 'index']);
+Route::get('/listings/{listing}', [ListingController::class, 'showListing']);
+
 
 Route::middleware(['auth:sanctum'])->group(function () {
   Route::post('/listings/create', [ListingController::class, 'store']);
   Route::get('/users', [UserController::class, 'index']);
-  Route::get('/user/{id}/{listings}', [ListingController::class, 'index']);
+  Route::get('/user/listings', [ListingController::class, 'getUserListings']);
+  Route::post('/user/listings/{listing}', [ListingController::class, 'update']);
+  Route::delete('/user/listings/{listing}', [ListingController::class, 'destroy']);
 });

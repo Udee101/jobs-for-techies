209621b4d3aabbf0f8da508aa152e9d8c3bc36c4
Udeorah Frank <franklynudeorah@yahoo.com>
2023-04-12 02:27:12 +0100

    create register and login logic

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $request->validate([
+            'first_name' => 'required|string',
+            'last_name' => 'required|string',
+            'username' => 'required|string',
+            'email' => 'required|string|email|unique:users,email',
+            'phone' => 'required|string',
+            'password' => 'required|string|confirmed'
+        ]);
+
+        $user = User::create([
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
+            'username' => $request->username,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'password' => bcrypt($request->password)
+        ]);
+
+        $token = $user->createToken('jobs for techies token')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token
+        ], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'login' => 'required',
+            'password' => 'required',
+        ]);
+
+        $authVar = filter_var($request->login, FILTER_VALIDATE_EMAIL);
+
+        if ($authVar) {
+            $user = User::where('email', $request->login)->first();
+        } else {
+            $user = User::where('username', $request->login)->first();
+        }
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'message' => 'Invalid Credentials'
+            ], 401);
+        }
+
+        $token = $user->createToken('jobs for techies token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'Login Successful',
+            'user' => [
+                'id' => $user->id,
+                'fisrt_name' => $user->first_name,
+                'last_name' => $user->last_name,
+                'username' => $user->username,
+                'email' => $user->email,
+            ],
+            'token' => $token
+        ], 200);
+    }
+}

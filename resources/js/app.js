0057b7591cd74f0498b25250cc2b9af5532f57ed
Udeorah Frank <franklynudeorah@yahoo.com>
2023-04-15 21:48:17 +0100

    setup basic routing

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,16 @@
 import './bootstrap';
 import { createApp } from 'vue';
 import App from './src/App.vue';
+import router from './src/router';
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+import { faUser } from '@fortawesome/free-solid-svg-icons'
+
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faUser);
 
 const app = createApp(App);
+app.use(router);
+app.component('f-a-i', FontAwesomeIcon);
 app.mount('#app');

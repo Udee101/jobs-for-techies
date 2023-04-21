445eb1a0a952acdb5db0001515458fc67f848c52
Udeorah Frank <franklynudeorah@yahoo.com>
2023-04-21 18:35:46 +0100

    create categories section

---
+++ resources/js/components/CategoryBox.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="category-box">
+    <img :src="category.imgSrc" alt="image" class="py-1">
+    <p class="mb-1 text-md ">{{ category.title }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      category: {
+        type: Object
+      }
+    }
+  }
+</script>
+
+<style scoped>
+.category-box{
+  background: var(--color-5);
+  border-radius: 1rem;
+  display: flex;
+  flex-direction: column;
+  place-content: center;
+  place-items: center;
+  padding: 0.5rem;
+}
+.category-box>img{
+  width: 2.5rem;
+}
+
+</style>
--- resources/js/pages/LandingPage.vue
+++ resources/js/pages/LandingPage.vue
@@ -1,9 +1,69 @@
 <template>
-    <div>Hero section</div>
+  <hero-section></hero-section>
+
+	<section class="categories" id="#categories">
+		<div class="categories-container">
+			<p class="text-center text-xl pb-1 section-header">Categories</p>
+				
+			<div class="category">
+				<category-box
+					v-for="category in categories" 
+					:category="category"
+					:key="category.id"
+				></category-box>
+			</div>
+		</div>
+	</section>
+
 </template>
 
 <script>
-export default {};
+import HeroSection from '../components/HeroSection.vue';
+import CategoryBox from '../components/CategoryBox.vue';
+import MarketingImg from '../../img/marketing.svg'
+import PmImg from '../../img/pm.svg'
+import DesignImg from '../../img/design.svg'
+import HrImg from '../../img/hr.svg'
+import FinanceImg from '../../img/finance.svg'
+import BusinessImg from '../../img/business.svg'
+
+export default {
+	components: {
+		"hero-section": HeroSection,
+		"category-box": CategoryBox
+	},
+	setup() {
+		const categories = [
+			{
+				title: "Design & Development",
+				imgSrc: DesignImg
+			},
+			{
+				title: "Marketing & Communication",
+				imgSrc: MarketingImg
+			},
+			{
+				title: "Project Management",
+				imgSrc: PmImg
+			},
+			{
+				title: "Human Research & Development",
+				imgSrc: HrImg
+			},
+			{
+				title: "Finance Management",
+				imgSrc: FinanceImg
+			},
+			{
+				title: "Business & Consulting",
+				imgSrc: BusinessImg
+			},
+		]
+
+		return {categories}
+	},
+};
 </script>
 
-<style scoped></style>
+<style scoped>
+</style>

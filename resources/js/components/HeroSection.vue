6d16d1fcf788c5459dea4da7dde8f2031b5bbf21
Udeorah Frank <franklynudeorah@yahoo.com>
2023-04-21 18:34:37 +0100

    create hero page

---
+++ resources/js/components/HeroSection.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="hero" id="#home">
+    <div class="hero-intro text-center">
+      <p class="hero-text text-xxl">Get The <span class="text-color-1">Right Job </span>You Deserve
+      </p>
+
+      <p class="hero-sub-text text-md my-1">Lots of jobs &amp; Several candidates are registered</p>
+    </div>
+
+    <div class="hero-cta text-center">
+      <a href="#jobs" class="find-jobs font-bold hover-opacity" >Find Jobs</a>
+      <router-link :to="{name: 'register'}" class="sign-up hover-opacity">Become a member</router-link>
+    </div>
+  </section>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style scoped>
+</style>

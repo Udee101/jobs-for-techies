159886107bac8b790ff57b86d1f2734598a4ce3f
Udeorah Frank <franklynudeorah@yahoo.com>
2023-04-23 00:55:57 +0100

    create and style news letter section

---
+++ resources/js/components/LandingPage/NewsLetterSection.vue
@@ -0,0 +1,37 @@
+<template>
+  <section class="subscribe" id="subscribe">
+		<div class="subscribe-container">
+			<div class="icon mb-1">
+			
+				<f-a-i icon="fas fa-paper-plane" class="text-white news-letter"/>
+
+				<f-a-i icon="fas fa-at" size="xl" class="text-white icon-1"/>
+
+				<f-a-i icon="fas fa-envelope" size="xl" class="text-white icon-2"/>
+				
+			</div>
+
+			<div class="news-letter-msg-container my-1">
+				<div class="news-letter-msg text-white text-lg mt-1 mb-0-5">
+					<p>Never Want to Miss Any <span class="font-bold"> Job News?</span></p>
+				</div>
+
+				<div class="news-letter-form my-1">
+					<form action="">
+						<div class="input-group">
+							<input type="email" placeholder="Enter Email" class="text-color-3">
+							<button type="submit" class="btn-send text-white p-0-5 hover-opacity">Subscribe</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</section>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style scoped>
+</style>

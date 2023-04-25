1cf31860ae34f06b5b504fd5847e2d3e4071c54e
Udeorah Frank <franklynudeorah@yahoo.com>
2023-04-25 15:04:20 +0100

    add site icon and update site name

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,9 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/png" href="http://localhost:5173/resources/img/logo.png">
 
-  <title>Laravel</title>
+  <title>Jobs for Techies</title>
 
   <!-- Fonts -->
   <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
